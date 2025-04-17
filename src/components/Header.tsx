b484b7e5533f3995207421c444e0560814ddfe63
DeborahAleaKoch <alea-de@web.de>
2025-04-17 16:27:18 +0200

    bg-image fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,6 @@ import Logo from "./Logo";
 import Nav from "./Nav";
 import supabase from "../utils/supabase";
 
-
-
 const logOut = async () => {
 	const { error } = await supabase.auth.signOut();
 	if (error) {
@@ -15,7 +13,7 @@ const logOut = async () => {
 };
 
 const Header = () => {
-	const { isLoggedIn } = useContext(mainContext) ;
+	const { isLoggedIn } = useContext(mainContext);
 	return (
 		<div className=''>
 			<div className='flex justify-between px-8 pt-11 '>
@@ -34,8 +32,8 @@ const Header = () => {
 				)}
 			</div>
 
-			<div className='w-full'>
-				<div className=' bg-[url(./../public/img/Banner.svg)] object-cover bg-no-repeat text-4xl text-white text-center h-60 text-balance pt-11 leading-13'>
+			<div className='w-screen'>
+				<div className=' bg-[url(./../public/img/Banner.svg)] object-cover bg-no-repeat bg-cover text-4xl text-white text-center h-60 text-balance pt-11 leading-13 w-screen'>
 					Lassen Sie sich inspirieren, kochen Sie mit <br />
 					Leidenschaft und erleben Sie unvergessliche <br />
 					Momente bei Tisch .
