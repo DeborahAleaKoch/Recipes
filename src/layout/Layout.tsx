709e047734c9782fc2be4952a4364c4e01326ea1
DeborahAleaKoch <alea-de@web.de>
2025-07-10 14:26:31 +0200

    footer layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,11 +7,11 @@ const Layout = () => {
 	// const location = useLocation();
 
 	return (
-		<>
+		<div className='flex flex-col min-h-screen'>
 			<Header />
 			<Outlet />
 			<Footer />
-		</>
+		</div>
 	);
 };
 
